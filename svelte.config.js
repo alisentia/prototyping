1f2dbcf023808819cbef2d939b7c856bbcaa7f02
Alexander Lau <alexander.lau@isentia.com>
2023-09-07 18:03:27 +1000

    added docs folder for github

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			assets : "public",
-			pages : "public",
+			assets : "docs",
+			pages : "docs",
 		}),
         paths: {
             base : dev ? '' : process.env.BASE_PATH,
