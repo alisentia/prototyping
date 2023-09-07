96b5af4e30259f6362afd85e075ce8929bd0f76a
Alexander Lau <alexander.lau@isentia.com>
2023-09-07 17:28:14 +1000

    testing new configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 	"extends" : [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
+		'plugin:svelte/recommended',
 	],
 	"parser" : "@typescript-eslint/parser",
 	"parserOptions" : {
@@ -60,6 +61,13 @@ module.exports = {
 			"rules" : {
 				"no-undef" : "off",
 			},
+		},
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
 		},
 	],
 };
